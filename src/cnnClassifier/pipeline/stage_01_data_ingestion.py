41fb6b55bd15c5a31e7d55b42bc1a2d8406acf85
laharianegama <lahari.anegama@gmail.com>
2024-08-15 21:31:09 -0500

    dvcAdded

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,4 +1,4 @@
-from src.cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 
 from cnnClassifier import logger
